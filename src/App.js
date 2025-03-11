bbb72438b1d408b6e4eb83ae1fb766ee173bca36
Nithin Kumar Gude <95757938+GudeNithin@users.noreply.github.com>
2025-03-11 11:30:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import React,{useState,useEffect} from 'react'
-import EmojiData from './emoji.json'
+import React, { useState } from "react";
 import './App.css'
+import NUser from './NoUser.webp'
 
 const App = () => {
-  const [search,setSearch]=useState('');
-  const [data,setData]=useState([])
-  const handler=e=>{
-    setSearch(e.target.value);
-  }
+  const [search, setSearch] = useState("");
+  const [mdata, setMdata] = useState(null); // Initialize as null
 
-  useEffect(()=>{
-   const newData=EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()));
-   setData(newData)
-  },[search])
+  const handler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then((res) => res.json())
+      .then((data) => setMdata(data));
+  };
 
   return (
     <div>
       <center>
-        <h4 className="m-3">React Emoji Search</h4>
-        <input type="text" name="search" value={search} onChange={handler}/>
-      </center>
-      {data.map(emoji=><div>
-        <div className="card m-2">
-        <div className="card-body" onClick={()=>{navigator.clipboard.writeText(emoji.symbol);alert("Copied")}}>
-          {emoji.symbol} {emoji.title}
-        </div>
-      </div>
-      </div>)}
-    </div>
-  )
-}
+        <h3>Search Github User</h3>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            value={search}
+            onChange={handler}
+          />{" "}
+          <br />
+          <input className="btn btn-primary mt-2" type="submit" name="search" />
+        </form>
 
-export default App
+        {/* Display data only if mdata exists */}
+        {mdata && !mdata.message ? (
+          <div>
+            <div className="container">
+              <div className="profile">
+                <img src={mdata.avatar_url} alt="Avatar" className="avatar" />
+                <div className="user-info">
+                  <h4>{mdata.name}</h4>
+                  <p>Public Repos: {mdata.public_repos} <br/>
+                   Followers: {mdata.followers} <br/>
+                   Following: {mdata.following}
+                  </p>
+                  
+                </div>
+              </div>
+            </div> 
+          </div>
+        ) : (
+          mdata?.message && 
+          (
+            <div className="no-user">
+              <img src={NUser} alt="User Not Found" className="not-found-img" />
+              <p className="no-user">User not found. Try again!</p>
+            </div>
+          )
+        )}
+      </center>
+    </div>
+  );
+};
+
+export default App;
